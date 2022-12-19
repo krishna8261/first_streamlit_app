10ae0c0c9b047f798add5d4f538a67ba03d2ae02
krishna8261 <121016041+krishna8261@users.noreply.github.com>
2022-12-19 09:43:20 +0000

    Update streamli_app.py
    fixed_typo

--- streamli_app.py
+++ streamli_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.text('Breakfast Menu')
+streamlit.text('kale,spinach & rocket smootie')
+streamlit.text('hard-boiled free-range egg')
+streamlit.header('Omega 3 & Blueberry Oatmeal')
