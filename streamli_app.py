6f210b055c1f74e5b6d0d08c407f2208606590d2
krishna8261 <121016041+krishna8261@users.noreply.github.com>
2022-12-19 09:44:21 +0000

    Update streamli_app.py
    changes_reverted

--- streamli_app.py
+++ streamli_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('kale,spinach & rocket smootie')
 streamlit.text('hard-boiled free-range egg')
-streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
